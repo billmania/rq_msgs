dea8661de3d69489c045d7ed1925c70b5103e002
Bill Mania <bill@manialabs.us>
2024-11-08 10:06:18 -0600

    Implemented GPIOInput and GPIOOutput interfaces. (#14)
    https://github.com/billmania/rq_msgs/issues/13

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ package_name = 'rq_msgs'
 
 setup(
     name=package_name,
-    version='0.0.2',
+    version='4.0.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
